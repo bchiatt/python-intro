2350a745ebde6f0fdf3a62e7c513897279c1cc5e
Brian Hiatt <bchiatt>
2014-11-13 09:44:56 -0600

    initial setup

---
+++ intro.py
@@ -0,0 +1,28 @@
+# this is a comment
+
+x = 3
+y = 4.2
+z = 'hello'
+a = "world"
+b = x + y - x
+c = 8 / 3       # returns floating point result
+d = 8 // 3      # returns floored integer
+e = 8 % 3       # modulus works the same as in js
+f = 8 ** 3      # to the power of operator
+g = z + a       # string concat
+
+# you can multiply numbers and strings
+3 * 'un'        # prints 'ununun'
+
+# negative indexes count back from end of word
+word = 'python'
+word[0]         # prints 'p'
+word[-2]        # prints 'o'
+word[0:3]       # prints 'pyt'
+len(word)       # length of array
+word[:]         # copies the array
+
+print('C:\some\name')  # \n means new line and doesn't print
+print(r'C:\some\name') # r means print raw
+
+print(x, y, z, a, b, c, d, e, f, g)
